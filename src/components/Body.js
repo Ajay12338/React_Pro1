49a7035d0b8f3e047390c16bbabe4b689185218c
Ajay12338 <ajaysam12338@gmail.com>
2023-10-01 20:39:57 +0530

    Added filter btn to filter higher rated restaurant

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,15 +3,33 @@ import Card from "./Card";
 import { useState } from "react";
 
 const Body = () => {
+  let filter_arr;
   const [restaurantData, setRestaurantData] = useState(
     swiggy_obj.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
   );
-  const [inputData, setInputData] = useState("");
+  const [toggle_flag, setToggleFlag] = useState(true);
   return (
     <div id="body">
       <div id="search-box">
         <input type="text" />
-        <button>search</button>
+        <button
+          className={toggle_flag ? "filter-btn" : "filter-btn-change"}
+          onClick={() => {
+            if (toggle_flag) {
+              filter_arr = restaurantData.filter((curr) => {
+                return curr.info.avgRating >= 4;
+              });
+            } else {
+              filter_arr =
+                swiggy_obj.data.cards[2].card.card.gridElements.infoWithStyle
+                  .restaurants;
+            }
+            setRestaurantData(filter_arr);
+            setToggleFlag(!toggle_flag);
+          }}
+        >
+          Filter By High rating
+        </button>
       </div>
       <div className="cards">
         {restaurantData.map((val, index) => {
